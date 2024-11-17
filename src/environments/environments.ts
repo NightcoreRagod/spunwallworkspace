8413e0a8c9b103d97a91ea498664afe68a470958
roshinievangelene <roshinievangelene@gmail.com>
2024-11-17 11:24:46 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyAQGV8QsMCmzg7Kj61TDUO9hWERm7qQb0Y',
+      authDomain: 'projectspunwall.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'projectspunwall',
+      storageBucket: 'projectspunwall.firebasestorage.app',
+      messagingSenderId: '945379123043',
+      appId: '1:945379123043:web:e5dc8b4e2655053b55bd56',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyBdt_CZVBKXG1vt7113lA1FHHfCpPdHmoA',
+  debug_token: ''
+};
